b881dcd92133bdee1f52c5027f71fcfd81c70620
Efrat_Ifergan <efrat_ef@outlock>
2022-07-20 12:28:06 +0300

    Fixed untracked files

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl:"https://us-central1-butterfly-host.cloudfunctions.net/",
-  BUTTERFLY_HOST_API_KEY: "test-bfc4a19d-a4d4-4c82-8d55-dcd43a246a72"
-};
